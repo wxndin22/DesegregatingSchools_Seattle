598b0395cc69f7ba61bc0f9a8d4f38c8f02803ae
Allie Miller <144857048+millera31@users.noreply.github.com>
2025-05-23 15:12:24 -0700

    Add files via upload
    code to create website for final report

---
+++ deseg_scroll.js
@@ -0,0 +1,9 @@
+// Smooth scroll for nav links
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      target.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  
